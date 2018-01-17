779219ffaef028669bb3237ed26ee2abc7fb4990
charles yang <charlesyang@charles.local>
2018-01-17 14:06:58 +0800

    ds

--- app/index.js
+++ app/index.js
@@ -1,19 +1,19 @@
 var generators = require('yeoman-generator');
-
+console.log(generators,11000);
 module.exports = generators.Base.extend({
-  // 构造函数
-  constructor: function () {
-
-    // 调用父类构造函数
-    generators.Base.apply(this, arguments);
-
-    // 执行的时候接收 `--coffee` 参数
-    this.option('coffee'); 
-  },
-  method1: function () {
-    console.log('method 1 just ran');
-  },
-  method2: function () {
-    console.log('method 2 just ran');
-  }
-});
+    prompting: function () {
+      return this.prompt([{
+        type    : 'input',
+        name    : 'name',
+        message : 'Your project name',
+        default : this.appname // Default to current folder name
+      }, {
+        type    : 'confirm',
+        name    : 'cool',
+        message : 'Would you like to enable the Cool feature?'
+      }]).then(function (answers) {
+        this.log('app name', answers.name);
+        this.log('cool feature', answers.cool);
+      }.bind(this));
+    }
+  })

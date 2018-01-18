4ec46d562cb11518eacf447bd4037ac39c6c1cff
yangyi <yhxyangyi@163.com>
2018-01-18 11:10:35 +0800

    yo-bo 更新

--- app/index.js
+++ app/index.js
@@ -8,15 +8,53 @@ module.exports = class extends Generator {
       desc: 'Skips the welcome message',
       type: Boolean
     });
-    console.log(this.argument,'argument');
+    console.log(this.argument, 'argument');
   }
-  prompting(){
-    console.log(1223);
+  prompting() {
+    const prompts = [{
+      type: 'checkbox',
+      name: 'libs',
+      message: '请选择要搭建的脚手架',
+      choices: [{
+        name: 'react-web',
+        value: 'web',
+        checked: true
+      }, {
+        name: 'react-apollo',
+        value: 'apollo',
+        checked: true
+      }, {
+        name: ' react-mobile',
+        value: 'mobile',
+        checked: true
+      }]
+    }];
+    return this.prompt(prompts).then(answers => {
+      console.log(answers,'9900');
+      const libs = answers.libs;
+      const hasLibs = flag => libs && !libs.includes(flag);
+      console.log(libs);
+      this.includeWeb = hasLibs('web');
+      this.includeApollo = hasLibs('apollo');
+      this.includeMobile = hasLibs('mobile');
+    });
   }
-  writing(){
-    console.log('write');
+  writing() {
+    console.log(this.includeWeb,this.includeApollo,this.includeMobile);
+    let from='./react-web',to='';
+    if(this.includeWeb){
+      from ='./react-web';
+    }
+    if(this.includeApollo){
+      from ='./react-apollo';
+    }
+    if(this.includeMobile){
+      from='./react-mobile'
+    }
+    this.fs.copy(this.templatePath(from),this.destinationPath(to));
+    console.log('移动成功');
   }
-  end (){
+  end() {
     console.log("end");
   }
 }
---
+++ app/templates/react-apollo/apollo.js
@@ -0,0 +1,1 @@
+let temp='hello appllo';
